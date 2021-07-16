9ac323d7f3947ac586436288c3580a2eac32e182
shanal purohit <shanalkpurohit@gmail.com>
2021-07-16 19:38:41 +0530

    ui updated

--- src/App.js
+++ src/App.js
@@ -145,7 +145,7 @@ class App extends Component {
   render() {
     return (
       <main className="content">
-        <h1 className="text-black text-uppercase text-center my-4">Task Manager</h1>
+        <h1 className="text-black text-uppercase text-center my-4">Todo App</h1>
         <div className="row ">
           <div className="col-md-6 col-sm-10 mx-auto p-0">
             <div className="card p-3">
